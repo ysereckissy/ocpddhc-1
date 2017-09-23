b40a7350bbe1ddac59b5b775c57745154b6e4fc1
Yannick Avelin Sereckissy-Namboy <yannick.sereckissy@gmail.com>
2017-09-23 14:09:05 -0400

    change laravel to OCPDDHC in the main page
    This commit changes the word Laravel into OCPDDHC in the main
    view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   OCPDDHC 
                 </div>
 
                 <div class="links">
